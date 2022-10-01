502367cdf0050fb0564674da40ae479eb593ad62
Galeno de Melo <galenodemelo@gmail.com>
2022-10-01 16:55:36 -0300

    feat: Organizando itens no Grid

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,3 +1,4 @@
+import Cta from "@components/cta/Cta";
 import Image from "next/image";
 import styles from "./Hero.module.scss";
 
@@ -7,6 +8,23 @@ export default function Hero() {
             <div className={styles.logo}>
                 <Image src="/img/logo-shift.svg" alt="Logotipo do Shift Festival" layout="fill" objectFit="contain" />
             </div>
+
+            <div className={styles.infoWrapper}>
+                <div className={styles.info}>
+                    <time dateTime="2022-12-03 09:00" className={styles.date}>
+                        03.dezembro.2022
+                    </time>
+                    <address className={styles.location}>
+                        <span>Joinville - SC</span>
+                        <hr className={styles.separator} />
+                        <span>Ágora Tech Park</span>
+                    </address>
+                </div>
+            </div>
+
+            <Cta>
+                Garanta sua vaga
+            </Cta>
         </section>
     )
 }
