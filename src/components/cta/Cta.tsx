79cc4067bbf6430b17e5f6ada5b4bc8f5a4eb7ac
Galeno de Melo <galenodemelo@gmail.com>
2022-10-03 19:26:08 -0300

    feat: Adicionando conteúdo intercalável da CTA

--- src/components/cta/Cta.tsx
+++ src/components/cta/Cta.tsx
@@ -1,3 +1,4 @@
+import ArrowIcon from "@components/arrowicon/ArrowIcon";
 import styles from "./Cta.module.scss";
 
 type Props = {
@@ -7,6 +8,14 @@ type Props = {
 export default function Cta(props: Props) {
     return (
         <button className={[styles.cta, styles[props.color]].join(" ")}>
+            <div className={styles.unhovered}>
+                <b>Garanta sua vaga</b>
+            </div>
+
+            <div className={styles.hovered}>
+                <span>Passaporte <data value="119.00">R$ 119</data></span>
+                <ArrowIcon color="red" />
+            </div>
         </button>
     )
 }
