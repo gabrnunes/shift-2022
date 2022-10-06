10bd03d19f2a72d73a3bd6dcb054af544c6654b3
Galeno de Melo <galenodemelo@gmail.com>
2022-10-06 20:37:56 -0300

    feat: Adicionando título do box

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -3,6 +3,11 @@ import styles from "./Intro.module.scss"
 export default function Intro() {
     return (
         <section className={styles.introWrapper}>
+            <h1 className={styles.headline}>
+                Um <em className={styles.circled}>festival</em> de<br />
+                <em className={styles.glowed}>criatividade</em>, inovação,<br />
+                futuro e <em className={styles.underlined}>sociedade</em>
+            </h1>
         </section>
     )
 }
