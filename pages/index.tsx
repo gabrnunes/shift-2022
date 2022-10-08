bf374c10e3f1e76ad8d67f0c6b00e59011a84b78
Gabriel Nunes <gnunesmail@gmail.com>
2022-10-08 16:59:41 -0300

    feat: componente programação

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Hero from "@components/hero/Hero";
 import Illustration from "@components/illustration/Illustration";
 import Intro from "@components/intro/Intro";
+import Schedule from "@components/schedule/Schedule";
 
 export default function IndexController() {
     return (
@@ -8,6 +9,7 @@ export default function IndexController() {
             <Hero />
             <Illustration />
             <Intro />
+            <Schedule />
         </>
-    )
+    );
 }
