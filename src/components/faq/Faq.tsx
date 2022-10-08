dd1c8afa20f59a13a055cadcb9560794937d24ea
Galeno de Melo <galenodemelo@gmail.com>
2022-10-08 11:58:38 -0300

    feat: Estilizando heading e o container

--- src/components/faq/Faq.tsx
+++ src/components/faq/Faq.tsx
@@ -1,8 +1,15 @@
 import styles from "./Faq.module.scss";
+import FaqQuestion from "./question/FaqQuestion";
 
 export default function Faq() {
     return (
         <section className={styles.faq}>
+            <h2 className={styles.heading} title="Meu pau no teu ouvido 😎">
+                Duvida? Duvido!
+            </h2>
+
+            <div className={styles.questionsWrapper}>
+            </div>
         </section>
     )
 }
