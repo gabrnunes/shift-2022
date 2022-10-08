4f34e55ceb33eec5de95a3b133236f7a0c4d12fa
Galeno de Melo <galenodemelo@gmail.com>
2022-10-08 11:00:27 -0300

    feat: Adição dos textos

--- src/components/info/JoinTheChange.tsx
+++ src/components/info/JoinTheChange.tsx
@@ -4,6 +4,17 @@ export default function JoinTheChange() {
     return (
         <section className={styles.info}>
             <div className={styles.text}>
+                <h2 className={styles.headline}>
+                    Participe da <em>mudança</em>
+                </h2>
+
+                <p className={styles.textBlock}>
+                    A terceira edição do Sh*ft Festival acontece no dia 03 de dezembro de 2022, no <strong>Ágora Tech Park</strong>, em Joinville.
+                </p>
+
+                <p className={styles.textBlock}>
+                    Os passaportes iniciam em R$ 119 e, com você, nosso evento será ainda mais incrível!
+                </p>
             </div>
 
             <div className={styles.photo}>
