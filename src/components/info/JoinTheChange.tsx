c72bf8dc7977c803e21b804f9d82059bf901e2f2
Galeno de Melo <galenodemelo@gmail.com>
2022-10-08 10:59:57 -0300

    feat: Organização do Grid

--- src/components/info/JoinTheChange.tsx
+++ src/components/info/JoinTheChange.tsx
@@ -3,6 +3,12 @@ import styles from "./JoinTheChange.module.scss";
 export default function JoinTheChange() {
     return (
         <section className={styles.info}>
+            <div className={styles.text}>
+            </div>
+
+            <div className={styles.photo}>
+            </div>
+
             <Cta color="red" />
         </section>
     )
